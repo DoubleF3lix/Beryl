d1540ec13cdf62ce51a1b7b0d4a5e2e2b0ace01c
DoubleFelix <48461905+DoubleF3lix@users.noreply.github.com>
2021-01-26 10:37:15 -0600

    Added support for multiple files, uses regex baloney that I don't understand to do other cool stuff.
    Thanks d.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-import subprocess
+import re as regex
 
 from PySide2.QtUiTools import QUiLoader
 from PySide2.QtWidgets import QApplication
@@ -14,47 +14,211 @@ loader = QUiLoader()
 window = loader.load(uiFile)
 uiFile.close()
 
+selectedFiles = None
 
 def select_function_file():
+    global selectedFiles
     selected_function_file = QFileDialog.getOpenFileNames(filter="Minecraft Function Files (*.mcfunction)")
-    function_path = ' '.join(selected_function_file[0])
-    window.function_path_text.setText(function_path)
-
+    selectedFiles = selected_function_file[:-1][0]
+    window.function_path_text.setText(' '.join(selectedFiles))
 
 def modify_function():
-    # Get the lines in the file
-    with open(window.function_path_text.text(), "r") as function:
-        lines = function.readlines()
+    for file in selectedFiles:
+        # Get the lines in the file
+        with open(file, "r") as function:
+            lines = function.readlines()
 
-    with open(window.function_path_text.text(), "w") as function:
-        whole_file = []
-        # Iterate through each line in the file
-        for line in lines:
-            modified_line = []  # Used for rebuilding the line
+        with open(file, "w") as function:
+            whole_file = []
+            # Iterate through each line in the file
+            for line in lines:
+                modified_line = []  # Used for rebuilding the line
 
-            # Split the line up and iterate through each element
-            split_line = line.split()
-            for index, value in enumerate(split_line):
-                if value == "positioned":
-                    x_value = int(split_line[index + 1])
-                    y_value = int(split_line[index + 2])
-                    z_value = int(split_line[index + 3])
+                # Split the line up and iterate through each element
+                split_line = line.split()
+                for index, value in enumerate(split_line):
+                    if value == "positioned":
+                        if split_line[index + 1] == "~":
+                            modified_line.append(value)
+                        else:
+                            x_value = float(split_line[index + 1])
+                            y_value = float(split_line[index + 2])
+                            z_value = float(split_line[index + 3])
+                            if x_value.is_integer():
+                                x_value = int(x_value)
+                            if y_value.is_integer():
+                                y_value = int(y_value)
+                            if z_value.is_integer():
+                                z_value = int(z_value)
 
-                    x_value += window.XOffset.value()
-                    y_value += window.YOffset.value()
-                    z_value += window.ZOffset.value()
+                            x_value += window.XOffset.value()
+                            y_value += window.YOffset.value()
+                            z_value += window.ZOffset.value()
 
-                    del split_line[index:index + 3]
-                    modified_line.append(f"positioned {x_value} {y_value} {z_value}")
-                else:
-                    modified_line.append(value)
-            whole_file.append(' '.join(modified_line))
-        function.write('\n'.join(whole_file))
+                            del split_line[index:index + 3]
+                            modified_line.append(f"positioned {x_value} {y_value} {z_value}")
+                
+                    elif value == "if":
+                        if split_line[index + 1] == "block":
+                            if split_line[index + 2] == "~":
+                                modified_line.append(value)
+                            else:
+                                x_value = float(split_line[index + 2])
+                                y_value = float(split_line[index + 3])
+                                z_value = float(split_line[index + 4])
+                                if x_value.is_integer():
+                                    x_value = int(x_value)
+                                if y_value.is_integer():
+                                    y_value = int(y_value)
+                                if z_value.is_integer():
+                                    z_value = int(z_value)
 
+                                x_value += window.XOffset.value()
+                                y_value += window.YOffset.value()
+                                z_value += window.ZOffset.value()
+
+                                del split_line[index:index + 4]
+                                modified_line.append(f"if block {x_value} {y_value} {z_value}")
+                        else:
+                            modified_line.append(value)
+     
+                    elif value == "unless":
+                        if split_line[index + 1] == "block":
+                            if split_line[index + 2] == "~":
+                                modified_line.append(value)
+                            else:
+                                x_value = float(split_line[index + 2])
+                                y_value = float(split_line[index + 3])
+                                z_value = float(split_line[index + 4])
+                                if x_value.is_integer():
+                                    x_value = int(x_value)
+                                if y_value.is_integer():
+                                    y_value = int(y_value)
+                                if z_value.is_integer():
+                                    z_value = int(z_value)
+
+                                x_value += window.XOffset.value()
+                                y_value += window.YOffset.value()
+                                z_value += window.ZOffset.value()
+
+                                del split_line[index:index + 4]
+                                modified_line.append(f"unless block {x_value} {y_value} {z_value}")
+                        else:
+                            modified_line.append(value)
+                    elif value == "summon":
+                        if split_line[index + 2] == "~":
+                            modified_line.append(value)
+                        else:
+                            id = split_line[index + 1]
+                            x_value = float(split_line[index + 2])
+                            y_value = float(split_line[index + 3])
+                            z_value = float(split_line[index + 4])
+                            if x_value.is_integer():
+                                x_value = int(x_value)
+                            if y_value.is_integer():
+                                y_value = int(y_value)
+                            if z_value.is_integer():
+                                z_value = int(z_value)
+
+                            x_value += window.XOffset.value()
+                            y_value += window.YOffset.value()
+                            z_value += window.ZOffset.value()
+
+                            del split_line[index:index + 4]
+                            modified_line.append(f"summon {id} {x_value} {y_value} {z_value}")
+
+                    elif value == "teleport":
+                        if split_line[index + 2] == "~":
+                            modified_line.append(value)
+                        else:
+                            entity = split_line[index + 1]
+                            x_value = float(split_line[index + 2])
+                            y_value = float(split_line[index + 3])
+                            z_value = float(split_line[index + 4])
+                            if x_value.is_integer():
+                                x_value = int(x_value)
+                            if y_value.is_integer():
+                                y_value = int(y_value)
+                            if z_value.is_integer():
+                                z_value = int(z_value)
+
+                            x_value += window.XOffset.value()
+                            y_value += window.YOffset.value()
+                            z_value += window.ZOffset.value()
+
+                            del split_line[index:index + 4]
+                            modified_line.append(f"teleport {entity} {x_value} {y_value} {z_value}")
+
+                    elif value == "setblock":
+                        if split_line[index + 1] == "~":
+                            modified_line.append(value)
+                        else:
+                            x_value = float(split_line[index + 1])
+                            y_value = float(split_line[index + 2])
+                            z_value = float(split_line[index + 3])
+                            if x_value.is_integer():
+                                x_value = int(x_value)
+                            if y_value.is_integer():
+                                y_value = int(y_value)
+                            if z_value.is_integer():
+                                z_value = int(z_value)
+
+                            x_value += window.XOffset.value()
+                            y_value += window.YOffset.value()
+                            z_value += window.ZOffset.value()
+
+                            del split_line[index:index + 3]
+                            modified_line.append(f"setblock {x_value} {y_value} {z_value}")
+
+                    elif value == "data":
+                        if split_line[index + 2] == "block":
+                            if split_line[index + 3] == "~":
+                                modified_line.append(value)
+                            else:
+                                action = split_line[index + 1]
+                                x_value = float(split_line[index + 3])
+                                y_value = float(split_line[index + 4])
+                                z_value = float(split_line[index + 5])
+                                if x_value.is_integer():
+                                    x_value = int(x_value)
+                                if y_value.is_integer():
+                                    y_value = int(y_value)
+                                if z_value.is_integer():
+                                    z_value = int(z_value)
+
+                                x_value += window.XOffset.value()
+                                y_value += window.YOffset.value()
+                                z_value += window.ZOffset.value()
+
+                                del split_line[index:index + 5]
+                                modified_line.append(f"data {action} block {x_value} {y_value} {z_value}")
+                        else: 
+                            modified_line.append(value)
+                    else: 
+                        modified_line.append(value)
+                whole_file.append(' '.join(modified_line))
+                function.write('\n'.join(whole_file))
+        
+        offset_dict = {
+            'x': window.XOffset.value(),
+            'y': window.YOffset.value(),
+            'z': window.ZOffset.value()
+        }
+
+        def add_offset(match):
+            start, dim, mid, value = match.group(1, 2, 3, 4)
+            value = float(value)
+            if value.is_integer():
+                value = int(value)
+            return f'{start}{dim}{mid}{value + offset_dict[dim]}'
+
+        pattern = r'([\[,]\s*)(x|y|z)(\s*\=\s*)(-?(\d*\.?\d+))'
+        function = regex.sub(pattern, add_offset, open(file, "r").read())
+
+        with open(file, "w") as _:
+            _.write(function)
     print("Finished building")
-
-
-subprocess.check_call([sys.executable, "-m", "pip", "install", "PySide2"])
+    
 window.XOffset.setRange(-2147483648, 2147483647)
 window.YOffset.setRange(-2147483648, 2147483647)
 window.ZOffset.setRange(-2147483648, 2147483647)
